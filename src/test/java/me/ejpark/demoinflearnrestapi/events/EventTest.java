f2252f9213e173177c44f00c446a3d95fc324ee1
4dv3ntur3 <allyep13.07@gmail.com>
2023-03-22 20:49:48 +0900

    Refactor: Apply JUnitParams in testFree, testOffline

--- src/test/java/me/ejpark/demoinflearnrestapi/events/EventTest.java
+++ src/test/java/me/ejpark/demoinflearnrestapi/events/EventTest.java
@@ -1,8 +1,13 @@
 package me.ejpark.demoinflearnrestapi.events;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(JUnitParamsRunner.class) // runner 설정
 public class EventTest {
 
     @Test // 메서드에 붙이는 것
@@ -46,71 +51,71 @@ public class EventTest {
 
     // 비즈니스 로직 테스트
     @Test
-    public void testFree() {
+    // parameter들의 집합. baseprice, maxprice, free값
+    // 3개 있으므로 3번 호출됨.
+    // test가 깨지면? 어떻게 꺠졌는지 보여 줌
+    // type safe하게 변경해야 함
+//    @Parameters({
+//            "0, 0, true",
+//            "100, 0, false",
+//            "0, 100, true"
+//    })
+//    @Parameters(method = "paramsForTestFree")
+    // javadoc 확인해 보기...
+    // 메서드 이름을 생략할 수 있을까? -> ParametersFor___
+    @Parameters
+    public void testFree(int basePrice, int maxPrice, boolean isFree) {
 
+        // 테스트 코드가 거의 똑같고 파라미터만 변경되는 형식이라면?
+        // 중복 제거 -> JunitParams 사용!
+        // junit은 원래 메서드 파라미터를 사용할 수 없는데, JunitParams는 메서드 파라미터를 사용할 수 있게 해 준다.
         // Given
         Event event = Event.builder()
-                .basePrice(0)
-                .maxPrice(0)
+                .basePrice(basePrice)
+                .maxPrice(maxPrice)
                 .build();
 
         // When
         event.update();
 
         // Then
-        assertThat(event.isFree()).isTrue();
+        assertThat(event.isFree()).isEqualTo(isFree);
 
+    }
 
-        // Given
-        event = Event.builder()
-                .basePrice(100)
-                .maxPrice(0)
-                .build();
-
-        // When
-        event.update();
-
-        // Then
-        assertThat(event.isFree()).isFalse(); // 무료 아님
-
-
-        // Given
-        event = Event.builder()
-                .basePrice(0)
-                .maxPrice(100)
-                .build();
-
-        // When
-        event.update();
-
-        // Then
-        assertThat(event.isFree()).isFalse(); // 무료 아님
+    // type safe version
+    private Object[] parametersForTestFree() { // ParametersFor{테스트 메서드 이름} 이 컨벤션임.
+        // 이렇게 명명할 경우 parameters에 method 인자 생략할 수 있다. 알아서 찾음.
+        return new Object[] {
+                new Object[] {0, 0, true},
+                new Object[] {100, 0, false},
+                new Object[] {0, 100, false},
+                new Object[] {100, 200, false}
+        };
     }
 
     // 온라인 오프라인 여부 테스트 (단위 테스트)
     @Test
-    public void testoffline() {
+    @Parameters // 이거 없으면 Junit으로 돌기 떄문에 꼭 붙여저야 함
+    public void testOffline(String location, boolean isOffline) {
         // Given
         Event event = Event.builder()
-                .location("강남역 네이버 D2 스타트업 팩토리")
+                .location(location)
                 .build();
 
         // When
         event.update();
 
         // Then
-        assertThat(event.isOffline()).isTrue(); // 장소 있으므로 오프라인
+        assertThat(event.isOffline()).isEqualTo(isOffline); // 장소 있으므로 오프라인
+    }
 
-
-        // Given
-        event = Event.builder()
-                .build();
-
-        // When
-        event.update();
-
-        // Then
-        assertThat(event.isOffline()).isFalse(); // 장소 없으므로 오프라인 아님
+    private Object[] parametersForTestOffline() {
+        return new Object[] {
+                new Object[] {"강남", true},
+                new Object[] {null, false},
+                new Object[] {"       ", false}
+        };
     }
 
 }
